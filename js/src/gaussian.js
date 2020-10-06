b030d12e32b0e20a0feb3384c3cbf294cac1ee70
Ilia Tretiak <ilia.tretiak.2001@mail.ru>
2020-10-07 00:38:21 +0300

    feat: add gaussian elimination function

---
+++ js/src/gaussian.js
@@ -0,0 +1,74 @@
+const abs = Math.abs;
+const EPS = 0.00000001;
+
+function getEmptyArray(length) {
+  const array = [];
+  for (let i = 0; i < length; i++) {
+    array.push(0);
+  }
+  return array;
+}
+
+function createCopy(matrix) {
+  const copy = [];
+  matrix.forEach(row => copy.push(row));
+  return copy;
+}
+
+function swapRows(matrix, rowNum1, rowNum2) {
+  const buffer = matrix[rowNum1];
+  matrix[rowNum1] = matrix[rowNum2];
+  matrix[rowNum2] = buffer;
+}
+
+function gaussianElimination(data) {
+  const matrix = createCopy(data);
+  const rowsLength = matrix.length;
+
+  for (let i = 0; i < rowsLength; i++) {
+    // Search for maximum in this column
+    let maxEl = abs(matrix[i][i]);
+    let rowWithMaxEl = i;
+    for (let j = i + 1; j < rowsLength; j++) {
+      if (abs(matrix[j][i]) > maxEl) {
+        maxEl = abs(matrix[j][i]);
+        rowWithMaxEl = j;
+      }
+    }
+
+    if (maxEl < EPS) {
+      throw new Error("solution doesn't exist");
+    }
+
+    // Swap row with maximum element with current row
+    if (rowWithMaxEl !== i) {
+      swapRows(matrix, rowWithMaxEl, i);
+    }
+
+    // Make all rows below this one equals 0 in the current column
+    for (let j = i + 1; j < rowsLength; j++) {
+      const c = -matrix[j][i] / matrix[i][i];
+      for (let k = i; k < rowsLength + 1; k++) {
+        if (i === k) {
+          matrix[j][k] = 0;
+        } else {
+          matrix[j][k] += c * matrix[i][k];
+        }
+      }
+    }
+  }
+
+  const answer = getEmptyArray(rowsLength);
+
+  // Solve equation Ax=b for an upper triangular matrix A
+  for (let i = rowsLength - 1; i >= 0; i--) {
+    answer[i] = matrix[i][rowsLength] / matrix[i][i];
+    for (let j = i - 1; j >= 0; j--) {
+      matrix[j][rowsLength] -= matrix[j][i] * answer[i];
+    }
+  }
+
+  return answer;
+}
+
+module.exports = gaussianElimination;
---
+++ js/src/gaussian.spec.js
@@ -0,0 +1,30 @@
+const gaussianElimination = require('./gaussian');
+const readMatrixFromFile = require('./service');
+
+describe('gaussianElimination function', ()=> {
+  let path = '';
+  test('should throw error', () => {
+    path = 'src/assets/matrix_without_solution-1.txt';
+    return readMatrixFromFile(path).then(matrix => {
+      expect( () => {
+        gaussianElimination(matrix)
+      }).toThrowError(new Error("solution doesn't exist"));
+    })
+  })
+
+  test('should throw error 2', () => {
+    path = 'src/assets/matrix_without_solution-2.txt'
+    return readMatrixFromFile(path).then(matrix => {
+      expect( () => {
+        gaussianElimination(matrix)
+      }).toThrowError(new Error("solution doesn't exist"));
+    })
+  })
+
+  test('should be equal to expected', () => {
+    const path = 'src/assets/data.txt';
+    const data = [[1, 2, 3], [4, 5, 6]];
+    const expected = [-1, 2];
+    expect(gaussianElimination(data)).toEqual(expected);
+  })
+})
